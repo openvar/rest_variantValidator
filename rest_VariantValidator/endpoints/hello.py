cbeeb9022ea326095934092c1d420be69f1f415a
e05374pc <e05374pc@b-losx4lhqf.home>
2023-01-04 16:32:10 +0000

    update copyright statement for 2023

--- rest_VariantValidator/endpoints/hello.py
+++ rest_VariantValidator/endpoints/hello.py
@@ -60,7 +60,7 @@ class HelloClass(Resource):
 
 
 # <LICENSE>
-# Copyright (C) 2016-2022 VariantValidator Contributors
+# Copyright (C) 2016-2023 VariantValidator Contributors
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
