5c75fc214d380c314580c7979a6ba04d97284bf8
Peter-J-Freeman <peter.j.freeman@manchester.ac.uk>
2023-11-10 14:27:32 +0000

    update object pool

--- rest_VariantValidator/utils/object_pool.py
+++ rest_VariantValidator/utils/object_pool.py
@@ -7,37 +7,57 @@ class ObjectPool:
         self.pool_size = pool_size
         self.objects = [object_type() for _ in range(pool_size)]
         self.lock = threading.Lock()
+        self.condition = threading.Condition(self.lock)
 
     def get_object(self):
-        with self.lock:
+        with self.condition:
             while not self.objects:
                 # Wait until an object becomes available
-                self.lock.release()
-                self.lock.acquire()
+                self.condition.wait()
             return self.objects.pop()
 
     def return_object(self, obj):
-        with self.lock:
+        with self.condition:
             self.objects.append(obj)
+            self.condition.notify()  # Notify waiting threads that an object is available
 
 class SimpleVariantFormatterPool:
     def __init__(self, pool_size=10):
         self.pool_size = pool_size
         self.pool = [simpleVariantFormatter for _ in range(pool_size)]
         self.lock = threading.Lock()
+        self.condition = threading.Condition(self.lock)
 
     def get(self):
-        with self.lock:
+        with self.condition:
             while not self.pool:
-                self.lock.release()
-                self.lock.acquire()
+                # Wait until a formatter becomes available
+                self.condition.wait()
             return self.pool.pop()
 
     def return_object(self, obj):
-        with self.lock:
+        with self.condition:
             self.pool.append(obj)
+            self.condition.notify()  # Notify waiting threads that a formatter is available
 
 # Create shared object pools
-vval_object_pool = ObjectPool(Validator, pool_size=2)
-g2t_object_pool = ObjectPool(Validator, pool_size=2)
-simple_variant_formatter_pool = SimpleVariantFormatterPool(pool_size=2)
+vval_object_pool = ObjectPool(Validator, pool_size=1)
+g2t_object_pool = ObjectPool(Validator, pool_size=1)
+simple_variant_formatter_pool = SimpleVariantFormatterPool(pool_size=1)
+
+# <LICENSE>
+# Copyright (C) 2016-2023 VariantValidator Contributors
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# </LICENSE>
