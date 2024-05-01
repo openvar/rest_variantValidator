a4f672fa95b2de2c814f730f84a49757dceb4787
John-F-Wagstaff <jfw16@leicester.ac.uk>
2024-05-01 11:39:01 +0100

    Add tests for the rate limiting code
    These are all unfortunately dependent on a somewhat fragile overwrite of
    the default test client behaviour, so may break with otherwise
    non-problematic code changes in the future. There does not seem to be a
    better way of doing these tests however, so we just have to tolerate
    this issue.

---
+++ tests/test_limiters.py
@@ -0,0 +1,51 @@
+"""Tests for the rate limiting code
+Currently limited to just testing that it works and keeps to the requested time.
+This code relies on the /hello/limit endpoint and relies on it's 1 access per-
+second limit.
+"""
+# Import necessary packages
+import time
+import pytest
+from flask import g
+from rest_VariantValidator.utils.limiter import limiter
+from rest_VariantValidator.app import application  # Import your Flask app
+
+# Fixture to set up the test client
+@pytest.fixture(scope='module',name='client')
+def rate_limit_test_client():
+    "Return a test client that works for rate limiting"
+    application.testing = False
+    application.debug = False
+    application.config['PROPAGATE_EXCEPTIONS'] = True
+    # This is fragile, and previous workarounds have failed before, so may
+    # break on updates to the limiter or flask test client code.
+    with application.app_context():
+        setattr(g, '%s_rate_limiting_complete' % limiter._key_prefix, False)
+    return application.test_client()  # Create a test client to interact with the app
+
+
+# test the limiter works as intended,
+def test_limit_endpoint_error_immediate(client):
+    "Provoke a limiter error by repeated requests to a limited endpoint"
+    response = client.get('/hello/limit', headers={'Content-Type': 'application/json'})
+    response = client.get('/hello/limit', headers={'Content-Type': 'application/json'})
+    assert response.status_code == 429
+    assert response.json["message"] == "Rate limit hit for this endpoint: See the endpoint documentation at https://rest.variantvalidator.org"
+
+def test_limit_endpoint_error_delayed(client):
+    """
+    Provoke a limiter error by repeated requests to a limited endpoint,
+    but with a partial (sub 1 second) delay.
+    """
+    response = client.get('/hello/limit', headers={'Content-Type': 'application/json'})
+    time.sleep(0.5)
+    response = client.get('/hello/limit', headers={'Content-Type': 'application/json'})
+    assert response.status_code == 429
+    assert response.json["message"] == "Rate limit hit for this endpoint: See the endpoint documentation at https://rest.variantvalidator.org"
+
+def test_limit_endpoint_success(client):
+    "Repeat the same as above, but with a 1 second delay to exceed limit"
+    response = client.get('/hello/limit', headers={'Content-Type': 'application/json'})
+    time.sleep(1)
+    response = client.get('/hello/limit', headers={'Content-Type': 'application/json'})
+    assert response.status_code == 200
